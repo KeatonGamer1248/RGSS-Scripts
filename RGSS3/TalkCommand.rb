4b97f76f5288cf68279a2f29f0fb88afea0683e0
KeatonGamer <124336768+KeatonGamer1248@users.noreply.github.com>
2024-08-17 16:21:43 -0700

    Create TalkCommand.rb
    Created "TalkCommand.rb"

---
+++ RGSS3/TalkCommand.rb
@@ -0,0 +1,58 @@
+=begin
+#//////////////////////////////////////////////////////////////////////////////#
+#                     TALK COMMAND SCRIPT BY KEATONGAMER                       #
+#//////////////////////////////////////////////////////////////////////////////#
+# Feel free to use for your projects, I don't mind!                            #
+# Credit would be nice though, hehe~ Maybe even a free copy of your game?      #
+#//////////////////////////////////////////////////////////////////////////////#
+=end
+
+$imported = {} unless $imported
+$imported[:KeatonGamer_TalkCommand] = true
+
+# Start Class
+class Scene_TalkCommand < Scene_MenuBase
+  
+  attr_accessor :command_talk
+  
+  def start
+    super
+    create_background
+    @actor = $game_party.menu_actor
+  end
+  
+  def create_background
+    @background_viewport = Viewport.new(146, 45, 130, 130)
+    @background_viewport.z = 150
+    
+    @background_sprite = Sprite.new()
+    @background_sprite.viewport = @background_viewport
+    
+    file = "Graphics/System/HUD_Talk"
+    @background_sprite.bitmap = Bitmap.new(file)
+    
+    update
+  end
+  
+  def on_actor_cancel
+    SceneManager.goto(Scene_Map)
+  end
+  
+  def dispose_background
+    @background_sprite.dispose
+  end
+  
+  def terminate
+    super
+    dispose_background
+  end
+  
+  def update
+    Graphics.update
+    Input.update
+  end
+  
+end
+
+instance = Scene_TalkCommand.new
+instance
