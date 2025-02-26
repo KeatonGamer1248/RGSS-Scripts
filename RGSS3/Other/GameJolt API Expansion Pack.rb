fc9a03982a8a5af101892cd1e8292b30f9d18d44
KeatonGamer <124336768+KeatonGamer1248@users.noreply.github.com>
2025-02-25 19:41:04 -0800

    Added GameJolt API Expansion Pack.rb.

---
+++ RGSS3/Other/GameJolt API Expansion Pack.rb
@@ -0,0 +1,39 @@
+#==============================================================================
+# ** GameJolt API Expansion Pack
+# Author: KeatonGamer
+# Version: v0.1.0-beta
+#------------------------------------------------------------------------------
+# This applies an expansion pack for Florian Van Strien's GameJolt API script.
+# Which can be donwloaded here on GameJolt: 
+# gamejolt.com/games/gamejolt-achievement-api-for-rpg-maker-vx-ace/40546
+#==============================================================================
+
+$imported = {} if $imported.nil?
+$imported["KG_GameJoltAPI-ExpansionPack"] = true
+
+module GameJolt
+  def self.session(value)
+
+    user_name = CustomData.get("gj_username")
+    user_token = CustomData.get("gj_usertoken")
+    
+    case value
+    when "open"
+      urlHash = ZOMD5.calc_md5("http://gamejolt.com/api/game/v1/" + "sessions/open/" + "?game_id=" + GameId + "&username=" + user_name + "&user_token=" + user_token + "&format=json" + PrivateKey)
+      urlHash = urlHash[0]
+      result = EFE.request("gamejolt.com", "api/game/v1/" + "sessions/open/" + "?game_id=" + GameId + "&username=" + user_name + "&user_token=" + user_token + "&format=json&signature=" + urlHash)
+      result = JSON.decode(result)
+      print(result)
+    when "close"
+      urlHash = ZOMD5.calc_md5("http://gamejolt.com/api/game/v1/" + "sessions/close/" + "?game_id=" + GameId + "&username=" + user_name + "&user_token=" + user_token + "&format=json" + PrivateKey)
+      urlHash = urlHash[0]
+      result = EFE.request("gamejolt.com", "api/game/v1/" + "sessions/close/" + "?game_id=" + GameId + "&username=" + user_name + "&user_token=" + user_token + "&format=json&signature=" + urlHash)
+      result = JSON.decode(result)
+    end
+    if result != nil
+      return result["response"]
+    else
+      return {"success" => "false"}
+    end
+  end
+end
